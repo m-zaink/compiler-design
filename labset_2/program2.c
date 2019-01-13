86825084cf61fccdbbf3282dcc609338ddb493f3
m-zaink <sidsmith9@gmail.com>
2019-01-13 10:08:42 +0530

    Reorganised into folders

--- labset_2/program2.c
+++ labset_2/program2.c
@@ -18,17 +18,19 @@ Process processes[100] ;  //Array of all processes
 int n ;  //Number of processes
 
 int findTotalBurstTime(){
+	//Function to find total burst time of all the processes combined.
 	int sum = 0 , i =0 ; 
 	for(i =0 ; i < n ; i++) sum+=processes[i].burstTime ; 
 	return sum ;
 }
 
 int sortCompare(const void * p ,const void * q){
+	//This function is the sort utility for Process array to be later used in.
 	return ((Process *)p)->arrivalTime - ((Process * )q)->arrivalTime ; 
 }
 
 
-bool areAllScheduled(){
+bool areAllProcessesScheduled(){
 	int i =0 ;
 	for(i =0 ; i < n ;i++) if(!processes[i].scheduled)return false ;
 	return true ; 
@@ -104,7 +106,9 @@ void scheduleProcesses(){
 	int time = 0 ; 
 	int order[100] , orderPointer =0 ; //This is the array which holds the gantt chart
 
-	while(!areAllScheduled()){ //loop until all processes are scheduled
+	while(!areAllProcessesScheduled())
+	{ 
+		//loop until all processes are scheduled
 		ProcessPair pair = getNextProcessToExecute(time) ; 
 
 		int current = pair.current, next = pair.next; 
