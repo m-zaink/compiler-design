2cebd2d9e5418f08a9089b316dc4737ffe0d23e5
m-zaink <sidsmith9@gmail.com>
2019-01-14 13:04:39 +0530

    Started labset_4 in java

---
+++ labset_2/java/Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Main {
+    private static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
