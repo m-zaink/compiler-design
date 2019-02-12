b7b39ccd467e5ef76d2776da8576a0d031faf2b3
nateshmbhat <nateshmbhat1@gmail.com>
2019-02-12 20:46:08 +0530

    updated ignore

---
+++ labset_9/cpp/nateshmbhat.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<ctype.h>
+#define MAX 256
+using namespace std ;
+int P ; //no of productions
+char productions[MAX][MAX]; // Array of production strings
+
+void addToResultSet(char target[] , char source[]){ //This function adds the elements(except epsilon) of a source set to the target set.
+	for(int i =0 ; i < MAX ; i++){
+		if(i=='#') continue ; 
+		target[i]+=source[i] ; 
+	}
+}
+
+void findFirst(char c , char result[] ){
+	if(islower(c)||c=='#'){ result[c]++ ; return ; }
+	char subResult[MAX] = { 0 }; 
+	for(int i =0 ;i < P ; i++){// traverse through all the productions 
+        if(productions[i][0]==c){//Non terminal on LHR matches with 'c'
+            for(int j =3 ; productions[i][j]!='\0' ; j++){ //Traverse from 4th character till end of string
+            	char symb = productions[i][j] ;
+            	if(islower(symb)){ //First(X) found for current production . add it to result . Break from traversing the string and go for the next production
+					result[symb]++ ; 
+					break; 
+            	}
+            	else{//its a non terminal 
+            		findFirst(symb , subResult) ; 
+					addToResultSet(result , subResult) ; 
+            		if(subResult['#']>0){//epsilon found.Continue traversing the production string
+						if(productions[i][j+1]=='\0') result['#']++ ; 
+            		}
+            		else break;
+            	}
+            }
+        }
+	}
+}
+
+int main(void){
+	cout<<"Enter number of productions : " ; cin>>P ;
+	cout<<"Enter the productions : " ;
+	for(int i =0 ;i <  P; i++) cin>>productions[i] ;
+	for(int i =0 ;i < P ; i++){
+        cout<< "First(" << productions[i][0]<< ") = "  ;
+        char result[MAX] = {0} ; 
+        findFirst(productions[i][0] , result) ;
+        for(int k =0 ; k < MAX ; k++){
+            if(result[k]>0) cout<<((char)k)<< " ";
+        }
+        cout<<endl;
+	}
+}
