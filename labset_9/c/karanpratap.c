d8427b3c8ad776aaa16c3741a97857ad582dcd8c
KP Singh <kps@localhost.localdomain>
2019-03-12 14:02:41 +0530

    Bug fix:Now checks whether terminal exists in the productions or not before calculating first for lowercase symbols

--- labset_9/c/karanpratap.c
+++ labset_9/c/karanpratap.c
@@ -1,6 +1,5 @@
 //Author : karanpratap
 //Program : First
-//WARNING : This program is in the making. Please don't refer it just yet.
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -9,16 +8,42 @@
 #include<stdbool.h>
 
 bool flag=false;
-bool last=false;
+int k=0;
+char terminals[100];
+
+bool in(char ch,char arr[100],int n){
+	int i;
+	for(i=0;i<n;i++)
+		if(arr[i]==ch)
+			return true;
+	return false;
+}
 
 bool first(char productions[100][100],char ch,int n){
-	
+	int i,j;
+	if(islower(ch) && in(ch,terminals,k)){
+		printf(" %c ",ch);
+		return false;
+	}
+	else if(ch=='#'){
+		return true;
+	}
+
+	for(i=0;i<n;i++){
+		if(productions[i][0]==ch){
+			for(j=3;productions[i][j]!='\0';j++){
+				if(first(productions,productions[i][j],n))
+					continue;	
+				else
+					return false;
+			}
+		}
+	}
 	return true;
 }
 
 int main(){
-	int n,i;
-
+	int n,i=0;
 	//input the productions
 	printf("\nEnter the number of productions:");
 	scanf("%d",&n);
@@ -27,13 +52,16 @@ int main(){
 	for(i=0;i<n;i++){
 		printf("\nEnter the production no %d:",i+1);
 		scanf("%s",productions[i]);
-	
+		int j=0;
+		for(j=0;j<strlen(productions[i]);j++){
+			if(islower(productions[i][j]) && !(in(productions[i][j],terminals,k)))
+				terminals[k++]=productions[i][j];
+		}
+		fflush(stdin);
 	}
-
 	getchar();
-	
 	//logic for first
-	
+	fflush(stdin);	
 	char ch='y';
 	do{
 		printf("\nEnter the symbol for which first is to be calculated:");
