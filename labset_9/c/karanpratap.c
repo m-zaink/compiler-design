50b59ba8c788aabe4f9828d8ae279fd506592f99
KP Singh <kps@localhost.localdomain>
2019-03-23 17:21:35 +0530

    Major Logical Bug Fixes:Works just fine now; still Bounty up in case of unnoticed logical errors; Documentation updated

--- labset_9/c/karanpratap.c
+++ labset_9/c/karanpratap.c
@@ -1,9 +1,9 @@
 //Author : karanpratap
 //Program : First of a given symbol in the given grammar
-//CAUTION : DONT REFER TO THIS CODE JUST YET! 
-//PROBLEM : EPSILON IS NOT DISPLAYED EVEN WHEN IT SHOULD BE; SUGGESTIONS ARE WELCOME!
-//			A FREE THUMS UP TO ANYONE WHO CAN FIGURE IT OUT!
-
+//UPDATE : INSTEAD OF DIRECTLY PRINTING, THE PROGRAM NOW STORES THE SYMBOLS IN FIRST OF A GIVEN 
+//		   SYMBOL TO A CHARACTER ARRAY.
+//NOTE : DO REFER AND TEST FOR ERRORS. FIRST PERSON TO POINT OUT A LOGICAL ERROR ALONG WITH FIX
+//		 GETS A FREE COKE
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -12,15 +12,19 @@
 #include<stdbool.h>
 
 /*
-flag - a boolean for checking the encounter of epsilon
-//k - a variable to keep count of the number of terminals
-//terminals - A character array to store all the terminals encountered in the given productions
+k - a variable to keep count of the number of terminals
+terminals - A character array to store all the terminals encountered in the given productions
+printChar - A character variable to store the current terminal to be printed in case it is
+encountered during calculation of first.
+firsts - A character array to store the symbols in first of the current symbol under consideration
+firstCounter - Number of first symbols found for the current symbol
 */
-bool flag=false;
 int k=0;
 char terminals[100];
-bool epsilon=false;
-char superChar;
+char printChar;
+char firsts[100];
+int firstCounter=0;
+bool flag=false;
 
 //This function checks whether a given terminal is already present in the terminals array
 bool in(char ch,char arr[100],int n){
@@ -33,9 +37,10 @@ bool in(char ch,char arr[100],int n){
 
 /*
 This is the recursive function where the first is calculated for a given symbol. Returns true if epsilon is encountered
-Operation : *If the input symbol is a terminal of the given grammar, its first will be the symbol itself.
-			*If the character is epsilon(here #), then we simply return true. We cant print epsilon just yet 
-			 as there may be more symbols following this symbol.
+Operation : *If the input symbol is a terminal of the given grammar, its first will be the symbol itself. This symbol is
+			added to the firsts if it is not already there.
+			*If the character is epsilon(here #), then we simply return true and make flag as true. We cant add epsilon 
+			just yet as there may be more symbols following this symbol.
 			*Otherwise, the symbol is a non-terminal It will check for this symbol's productions. If found, it
 			 checks the body part of its production and recursively calls itself on the symbols of that production,
 			 following these rules:
@@ -45,22 +50,36 @@ Operation : *If the input symbol is a terminal of the given grammar, its first w
 				-If a non-terminal is encountered, it calls itself recursively for that symbol and these steps are
 				 repeated.
 			*This finally results in all the symbols in the first of the requested symbol to be printed.
+			UPDATE: counter here is used to mark the depth of recursion; if counter==0, then this is the first function
+			call and recursion has not yet begun.
+			UPDATE: If we are currently in the first call and epsilon is encountered then we definitely need to add epsilon
+			to the first of the given symbol, that is what condition (*) does.
 */
-bool first(char productions[100][100],char ch,int n){
+bool first(char productions[100][100],char ch,int n,int counter){
 	int i,j;
 	if(islower(ch) && in(ch,terminals,k)){
-		printf(" %c ",ch);
+		if(!in(ch,firsts,firstCounter))
+			firsts[firstCounter++]=ch;
 		return false;
 	}
 	else if(ch=='#'){
+		flag=true;
 		return true;
 	}
 
 	for(i=0;i<n;i++){
 		if(productions[i][0]==ch){
 			for(j=3;productions[i][j]!='\0';j++){
-				if(first(productions,productions[i][j],n))
+				if(first(productions,productions[i][j],n,counter+1) && flag && counter==0 && j==strlen(productions[i])-1){ // *
+					if(!in('#',firsts,firstCounter))
+						firsts[firstCounter++]='#';
+					flag=false;
 					continue;
+				}
+				else if(first(productions,productions[i][j],n,counter+1) || flag){
+					flag=false;
+					continue;
+				}
 				else
 					break;
 			}
@@ -98,12 +117,15 @@ int main(){
 	do{
 		printf("\nEnter the symbol for which first is to be calculated:");
 		scanf("%c",&ch);
-		printf("First of %c: {",ch);
 		fflush(stdin);
-		first(productions,ch,n);
+		first(productions,ch,n,0);
+		int l;
+		printf("First of %c : {",ch);
+		for(l=0;l<firstCounter;l++)
+			printf(" %c ",firsts[l]);
+		printf("}\n");
+		firstCounter=0;
 		fflush(stdin);
-		printf(" }");
-		flag=false;
 		printf("\nEnter more?(y/n)");
 		getchar();
 		scanf("%c",&ch);
