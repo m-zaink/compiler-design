cb3f912d7506581dd18514b78d0847e8fa51716f
nateshmbhat <nateshmbhat1@gmail.com>
2019-05-22 19:45:42 +0530

    fixed a small bug in lab10

---
+++ labset_11/c/nateshmbhat.c
@@ -0,0 +1,73 @@
+ #include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+
+int numofproduct,k= 0  ; 
+char productionset[10][10],str[20];
+
+void recursiveDescent(char p)
+{
+	int found = 0 , temp , i , j  ; 
+
+	for(i =0 ;i < numofproduct ; i++ )
+	{
+		if(productionset[i][0]==p){
+			temp = k ; 
+			for(int j =3 ; productionset[i][j] ; j++){
+				while(str[k]=='#')k++ ; 
+				if(isupper(productionset[i][j])){
+					recursiveDescent(productionset[i][j]) ; 
+				}
+				else if(productionset[i][j] == str[k]){
+					k++ ; 
+				}
+				else if (productionset[i][j] == '#'){
+					break ; 
+				}
+				else{
+					k = temp ; 
+					break; 
+				}
+			}
+		}
+	}
+}
+
+
+int main(void)
+{
+	printf("Number of productions:");
+	scanf("%d",&numofproduct);
+	printf("Enter the productions , enter # for epsilon : \n");
+	for(int i=0;i<numofproduct;i++)
+		scanf("%s",productionset[i]);
+
+	while(1){
+		k = 0 ; 	
+		printf("\nEnter the string : ");
+		scanf("%s",str);
+
+		recursiveDescent(productionset[0][0]);
+
+		if(k==strlen(str))
+			printf("\n Input string is valid");
+		else
+			printf("\n Invalid string !!!");
+	}
+}
+
+
+/*
+
+E--TA
+A--+TA
+A--#
+T--FB
+B--*FB
+B--#
+F--(E)
+F--id
+
+
+*/
